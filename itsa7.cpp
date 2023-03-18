c005b9fdee6707bf59c539697495616a2acd7c3b
silas920426 <90045542+silas920426@users.noreply.github.com>
2023-03-19 03:19:23 +0800

    加入專案檔案。

---
+++ itsa7.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        int a1, a2, b1, b2;
+        char w;
+        cin >> w >> a1 >> a2 >> b1 >> b2;
+        switch (w) {
+        case '+':cout << a1 + b1 << " " << a2 + b2 << "\n"; break;
+        case '-':cout << a1 - b1 << " " << a2 - b2 << "\n"; break;
+        case '*':cout << a1 * b1 - a2 * b2 << " " << a2 * b1 + a1 * b2 << "\n"; break;
+        case '/':cout << (a1 * b1 + a2 * b2) / (b1 * b1 + b2 * b2) << " " << (a2 * b1 - a1 * b2) / (b1 * b1 + b2 * b2) << "\n"; break;
+        }
+    }
+}
